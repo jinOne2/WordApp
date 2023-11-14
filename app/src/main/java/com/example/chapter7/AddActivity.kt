6ac3c44d238e80a3197674b6a2c1739326e230e7
jinone <februarycl@naver.com>
2023-11-14 16:13:20 +0900

    완성

--- app/src/main/java/com/example/chapter7/AddActivity.kt
+++ app/src/main/java/com/example/chapter7/AddActivity.kt
@@ -1,14 +1,18 @@
 package com.example.chapter7
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.view.children
+import androidx.core.widget.addTextChangedListener
 import com.example.chapter7.databinding.ActivityAddBinding
 import com.google.android.material.chip.Chip
 
 class AddActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAddBinding
+    private var originWord : Word? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddBinding.inflate(layoutInflater)
@@ -16,7 +20,7 @@ class AddActivity : AppCompatActivity() {
 
         initViews()
         binding.addButton.setOnClickListener {
-            add()
+            if(originWord == null) add() else edit()
         }
     }
 
@@ -29,6 +33,24 @@ class AddActivity : AppCompatActivity() {
             types.forEach { text ->
                 addView(createChip(text))
             }
+        }
+
+        binding.textInputEditText.addTextChangedListener {
+            it?.let {text ->
+                binding.textTextInputLayout.error = when(text.length){
+                    0 -> "값을 입력 해주세요"
+                    1 -> "2자 이상을 입력해주세요"
+                    else -> null
+                }
+            }
+        }
+
+        originWord = intent.getParcelableExtra<Word>("originWord")
+        originWord?.let{word ->
+            binding.textInputEditText.setText(word.text)
+            binding.meanTextInputEditText.setText(word.mean)
+            val selectedChip = binding.typeChipGroup.children.firstOrNull{ (it as Chip).text == word.type} as? Chip
+            selectedChip?.isChecked = true
         }
     }
 
@@ -51,11 +73,30 @@ class AddActivity : AppCompatActivity() {
             AppDatabase.getInstance(this)?.wordDao()?.insert(word)
             runOnUiThread{
                 Toast.makeText(this, "저장을 완료했습니다.", Toast.LENGTH_LONG).show()
-                finish()
             }
+            val intent = Intent().putExtra("isUpdated", true)
+            setResult(RESULT_OK, intent)
+            finish()
         }.start()
 
 
 
+    }
+
+    private fun edit(){
+        val text = binding.textInputEditText.text.toString()
+        val mean = binding.meanTextInputEditText.text.toString()
+        val type = findViewById<Chip>(binding.typeChipGroup.checkedChipId).text.toString()
+        val editWord = originWord?.copy(text = text, mean = mean, type = type)
+
+        Thread{
+            editWord?.let {
+                AppDatabase.getInstance(this)?.wordDao()?.update(editWord)
+                val intent = Intent().putExtra("editWord", editWord)
+                setResult(RESULT_OK, intent)
+                runOnUiThread { Toast.makeText(this, "수정 완료",Toast.LENGTH_LONG).show() }
+                finish()
+            }
+        }.start()
     }
 }
--- app/src/main/java/com/example/chapter7/MainActivity.kt
+++ app/src/main/java/com/example/chapter7/MainActivity.kt
@@ -13,11 +13,22 @@ class MainActivity : AppCompatActivity(), WordAdapter.ItemClickListener {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var wordAdapter: WordAdapter
+    private var selectedWord: Word? = null
     private val updateAddWordResult = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
-    ){ result ->
-        if (result.resultCode == RESULT_OK){
-            val isUpdated = result.data?.getBooleanExtra("isUpdated", false) ?: false
+    ) { result ->
+        val isUpdated = result.data?.getBooleanExtra("isUpdated", false) ?: false
+        if (result.resultCode == RESULT_OK && isUpdated) {
+            updateAddWord()
+        }
+    }
+
+    private val updateEditWordResult = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        val editWord = result.data?.getParcelableExtra<Word>("editWord")
+        if (result.resultCode == RESULT_OK && editWord != null) {
+            updateEditWord(editWord)
         }
     }
 
@@ -28,23 +39,34 @@ class MainActivity : AppCompatActivity(), WordAdapter.ItemClickListener {
 
         initRecyclerView()
         binding.addButton.setOnClickListener {
-            startActivity(Intent(this, AddActivity::class.java))
+            Intent(this, AddActivity::class.java).let {
+                updateAddWordResult.launch(it)
+            }
+        }
+
+        binding.deleteImageView.setOnClickListener {
+            delete()
+        }
+
+        binding.editImageView.setOnClickListener {
+            edit()
         }
 
     }
 
 
-
-    private fun initRecyclerView(){
+    private fun initRecyclerView() {
         wordAdapter = WordAdapter(mutableListOf(), this)
-        binding.wordRecyclerView.apply{
+        binding.wordRecyclerView.apply {
             adapter = wordAdapter
-            layoutManager = LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
-            val dividerItemDecoration = DividerItemDecoration(applicationContext, LinearLayoutManager.VERTICAL)
+            layoutManager =
+                LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
+            val dividerItemDecoration =
+                DividerItemDecoration(applicationContext, LinearLayoutManager.VERTICAL)
             addItemDecoration(dividerItemDecoration)
         }
 
-        Thread{
+        Thread {
             val list = AppDatabase.getInstance(this)?.wordDao()?.getAll() ?: emptyList()
             wordAdapter.list.addAll(list)
             runOnUiThread { wordAdapter.notifyDataSetChanged() }
@@ -52,8 +74,61 @@ class MainActivity : AppCompatActivity(), WordAdapter.ItemClickListener {
 
     }
 
+
+    private fun updateAddWord() {
+        Thread {
+            AppDatabase.getInstance(this)?.wordDao()?.getLatestWord()?.let { word ->
+                wordAdapter.list.add(0, word)
+                runOnUiThread { wordAdapter.notifyDataSetChanged() }
+            }
+
+        }.start()
+    }
+
+    private fun delete() {
+        if (selectedWord == null) return
+
+
+        Thread {
+            selectedWord?.let { word ->
+                AppDatabase.getInstance(this)?.wordDao()?.delete(word)
+                runOnUiThread {
+                    wordAdapter.list.remove(word)
+                    wordAdapter.notifyDataSetChanged()
+                    binding.textTextView.text = ""
+                    binding.meanTextView.text = ""
+                    Toast.makeText(this, "삭제 완료", Toast.LENGTH_LONG).show()
+                }
+
+            }
+
+        }.start()
+    }
+
+    private fun updateEditWord(word: Word) {
+        val index = wordAdapter.list.indexOfFirst { it.id == word.id }
+        wordAdapter.list[index] = word
+        runOnUiThread {
+            selectedWord = word
+            wordAdapter.notifyItemChanged(index)
+            binding.textTextView.text = word.text
+            binding.meanTextView.text = word.mean
+
+        }
+    }
+
+    private fun edit() {
+        if (selectedWord == null) return
+
+        val intent = Intent(this, AddActivity::class.java).putExtra("originWord", selectedWord)
+        updateEditWordResult.launch(intent)
+    }
+
     override fun onClick(word: Word) {
-        Toast.makeText(this, "${word.text}가 클릭 됐습니다.", Toast.LENGTH_LONG).show()
+        selectedWord = word
+        binding.textTextView.text = word.text
+        binding.meanTextView.text = word.mean
+
     }
 
 
